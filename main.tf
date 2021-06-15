9392b6f6d178846638c1f35a03632997e7cf4c16
Hein Moe Thu <thu105@mail.chapman.edu>
2021-06-15 03:20:51 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.72.0"
+    }
+  }
+}
+
 provider "google" {
   project = "{{YOUR GCP PROJECT}}"
   region  = "us-central1"
