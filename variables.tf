8375e36e94ee4741eb578cb5a323c0a2920d9651
Hein Moe Thu <thu105@mail.chapman.edu>
2021-06-16 22:43:04 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,6 +23,16 @@ variable "network_name" {
   default     = "vpc-network"
 }
 
+variable "ip_range_pods_name" {
+  description = "The secondary ip range to use for pods"
+  default     = "ip-range-pods"
+}
+
+variable "ip_range_services_name" {
+  description = "The secondary ip range to use for services"
+  default     = "ip-range-svc"
+}
+
 variable "peering_address_name" {
   description = "The VPC network address name for peering"
   default     = "vpc-peering-address"
