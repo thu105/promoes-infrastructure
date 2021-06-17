c85f3a6cf65170e86ee25cccabbb824751b160a9
Hein Moe Thu <thu105@mail.chapman.edu>
2021-06-16 22:29:04 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "project_id" {
 
 variable "cluster_name" {
   description = "The name for the GKE cluster"
-  default     = "gke-on-vpc-cluster"
+  default     = "gke-cluster-1"
 }
 
 variable "region" {
@@ -20,20 +20,5 @@ variable "zone" {
 
 variable "network" {
   description = "The VPC network created to host the cluster in"
-  default     = "gke-network"
-}
-
-variable "subnetwork" {
-  description = "The subnetwork created to host the cluster in"
-  default     = "gke-subnet"
-}
-
-variable "ip_range_pods_name" {
-  description = "The secondary ip range to use for pods"
-  default     = "ip-range-pods"
-}
-
-variable "ip_range_services_name" {
-  description = "The secondary ip range to use for services"
-  default     = "ip-range-svc"
+  default     = "vpc-network"
 }
